b20b055322c9e508a9cff55660b6ebb147f4ca91
Barış Gündüz <baris@gunduzmedya.com>
2023-04-03 16:02:37 +0300

    Feature - Delete Logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,10 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
+import {
+    GET_LOGS,
+    SET_LOADING,
+    LOGS_ERROR,
+    ADD_LOG,
+    DELETE_LOG,
+} from "./types";
 
 /* export const getLogs = () => {
     return async (dispatch, getState) => {
@@ -31,23 +37,39 @@ export const getLogs = () => async (dispatch) => {
     }
 };
 
-// ADd new log
+// Add new log
 export const addLog = (log) => async (dispatch) => {
     try {
         setLoading();
 
         const res = await fetch("/logs", {
-            method: 'POST',
+            method: "POST",
             body: JSON.stringify(log),
             headers: {
-                'Content-Type': 'application/json'
-            }
+                "Content-Type": "application/json",
+            },
         });
         const data = await res.json();
 
         dispatch({
             type: ADD_LOG,
             payload: data,
+        });
+    } catch (err) {
+        dispatch({ type: LOGS_ERROR, payload: err.response.data });
+    }
+};
+
+// Delete log from server
+export const deleteLog = (id) => async (dispatch) => {
+    try {
+        setLoading();
+
+        await fetch(`/logs/${id}`, { method: "DELETE" });
+
+        dispatch({
+            type: DELETE_LOG,
+            payload: id,
         });
     } catch (err) {
         dispatch({ type: LOGS_ERROR, payload: err.response.data });
