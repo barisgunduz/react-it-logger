e081c3de96575059f37cac0085e7cdec42b45602
Barış Gündüz <baris@gunduzmedya.com>
2023-04-03 17:43:36 +0300

    Feature - Add Technicians

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -1,4 +1,10 @@
-import { GET_TECHS, ADD_TECH, DELETE_TECH, SET_LOADING, TECHS_ERROR } from "./types";
+import {
+    GET_TECHS,
+    ADD_TECH,
+    DELETE_TECH,
+    SET_LOADING,
+    TECHS_ERROR,
+} from "./types";
 
 // Get techs from server
 export const getTechs = () => async (dispatch) => {
@@ -16,6 +22,30 @@ export const getTechs = () => async (dispatch) => {
         dispatch({ type: TECHS_ERROR, payload: err.response.statusText });
     }
 };
+
+// Add technician to server
+export const addTech = (tech) => async (dispatch) => {
+    try {
+        setLoading();
+
+        const res = await fetch("/techs", {
+            method: "POST",
+            body: JSON.stringify(tech),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await res.json();
+
+        dispatch({
+            type: ADD_TECH,
+            payload: data,
+        });
+    } catch (err) {
+        dispatch({ type: TECHS_ERROR, payload: err.response.statusText });
+    }
+};
+
 // Set loading to true
 export const setLoading = () => {
     return {
